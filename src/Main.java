ec60111788bbbd44a922969bdb9a554549c2a6a1
todorbalabanov <todorbalabanov@192.168.0.101>
2017-07-05 18:27:28 +0300

    Initial revision.

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * Application single entry point.
+ * 
+ * @author Todor Balabanov
+ */
+public class Main {
+
+	/**
+	 * Single entry point method.
+	 * 
+	 * @param args Command line arguments.
+	 */
+	public static void main(String[] args) {
+	}
+}
