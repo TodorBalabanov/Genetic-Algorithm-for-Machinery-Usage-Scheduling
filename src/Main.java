614e51d2dd5590b3d977de3eb6e1396971af6ac7
FarbyPeckers <mihailernandes@gmail.com>
2017-07-06 12:52:47 +0300

    Added 'Action' class getters and setters + main method fixes.

--- src/Main.java
+++ src/Main.java
@@ -59,37 +59,37 @@ class Action {
 	/**
 	 * Action start time.
 	 */
-	int start = 0;
+	private int start = 0;
 
 	/**
 	 * Action duration.
 	 */
-	int duration = 0;
+	private int duration = 0;
 
 	/**
 	 * Action end time.
 	 */
-	int end = 0;
+	private int end = 0;
 
 	/**
 	 * Action status.
 	 */
-	boolean done = false;
+	private boolean done = false;
 
 	/**
 	 * Machine used reference.
 	 */
-	Machine machine = null;
+	private Machine machine = null;
 
 	/**
 	 * Operation belongs reference.
 	 */
-	Operation operation = null;
+	private Operation operation = null;
 
 	/**
 	 * Reference to the previous action.
 	 */
-	Action previous = null;
+	private Action previous = null;
 
 	/**
 	 * Constructor with all parameters,
@@ -130,6 +130,104 @@ class Action {
 		return "Action [start=" + start + ", duration=" + duration + ", end=" + end + ", done=" + done + ", machine="
 				+ machine + "]";
 	}
+
+	/**
+	 * Start field getter
+	 */
+	public int getStart() {
+		return start;
+	}
+
+	/**
+	 * Start field setter
+	 */
+	public void setStart(int start) {
+		this.start = start;
+	}
+
+	/**
+	 * Duration field getter
+	 */
+	public int getDuration() {
+		return duration;
+	}
+
+	/**
+	 * Duration field setter
+	 */
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	/**
+	 * End field getter
+	 */
+	public int getEnd() {
+		return end;
+	}
+
+	/**
+	 * End field setter
+	 */
+	public void setEnd(int end) {
+		this.end = end;
+	}
+
+	/**
+	 * Done field getter
+	 */
+	public boolean isDone() {
+		return done;
+	}
+
+	/**
+	 * Done field setter
+	 */
+	public void setDone(boolean done) {
+		this.done = done;
+	}
+
+	/**
+	 * Machine field getter
+	 */
+	public Machine getMachine() {
+		return machine;
+	}
+
+	/**
+	 * Machine field setter
+	 */
+	public void setMachine(Machine machine) {
+		this.machine = machine;
+	}
+
+	/**
+	 * Operation field getter
+	 */
+	public Operation getOperation() {
+		return operation;
+	}
+
+	/**
+	 * Operation field setter
+	 */
+	public void setOperation(Operation operation) {
+		this.operation = operation;
+	}
+
+	/**
+	 * Previous field getter
+	 */
+	public Action getPrevious() {
+		return previous;
+	}
+
+	/**
+	 * Previous field setter
+	 */
+	public void setPrevious(Action previous) {
+		this.previous = previous;
+	}
 }
 
 /**
@@ -143,7 +241,7 @@ class Operation {
 	 */
 	String name = "";
 
-	//TODO May be LinkedList is better choice for this member field.
+	// TODO May be LinkedList is better choice for this member field.
 	/**
 	 * List of actions taken for this operation.
 	 */
@@ -158,7 +256,7 @@ class Operation {
 	 * Reference to the previous operation.
 	 */
 	Operation previous = null;
-	
+
 	/**
 	 * Constructor with all parameters.
 	 * 
@@ -190,16 +288,17 @@ class Operation {
  * @author Todor Balabanov
  */
 class Job {
+
 	/**
 	 * Job title.
 	 */
-	String name = "";
+	private String name = "";
 
-	//TODO May be LinkedList is better choice for this member field.
+	// TODO May be LinkedList is better choice for this member field.
 	/**
 	 * List of operations taken for this job.
 	 */
-	List<Operation> operations = new ArrayList<Operation>();
+	private List<Operation> operations = new ArrayList<Operation>();
 
 	/**
 	 * Constructor with all parameters.
@@ -219,6 +318,34 @@ class Job {
 	@Override
 	public String toString() {
 		return "Job [name=" + name + ", operations=" + operations + "]";
+	}
+	
+	/**
+	 * Name field getter
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * Name field setter
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * Operation list getter
+	 */
+	public List<Operation> getOperations() {
+		return operations;
+	}
+	
+	/**
+	 * Operation list setter
+	 */
+	public void setOperations(List<Operation> operations) {
+		this.operations = operations;
 	}
 }
 
@@ -281,8 +408,8 @@ public class Main {
 				while (i < data.length
 						&& (job.name.equals(data[i][0].toString()) || data[i][0].toString().equals(""))) {
 					Operation previous = null;
-					if(job.operations.size() > 0) {
-						previous = job.operations.get( job.operations.size()-1 );
+					if (job.operations.size() > 0) {
+						previous = job.operations.get(job.operations.size() - 1);
 					}
 					job.operations.add(new Operation(data[i][1].toString(), job));
 					i++;
@@ -326,8 +453,8 @@ public class Main {
 		for (Job job : jobs) {
 			for (Operation operation : job.operations) {
 				for (Action action : operation.actions) {
-					action.start = min + PRNG.nextInt(max - min + 1);
-					action.end = action.start + action.duration;
+					action.setStart(min + PRNG.nextInt(max - min + 1));
+					action.setEnd(action.getStart() + action.getDuration());
 				}
 			}
 		}
@@ -352,10 +479,10 @@ public class Main {
 				/*
 				 * It is time the action to be done.
 				 */
-				if (action.start == time && action.done == false) {
-					if (action.machine.occupied == false) {
-						action.machine.occupied = true;
-						action.machine.action = action;
+				if (action.getStart() == time && action.isDone() == false) {
+					if (action.getMachine().isOccupied() == false) {
+						action.getMachine().isOccupied() = true;
+						action.getMachine().getAction() = action;
 					} else {
 						System.err.println("Schedule collision for: " + action);
 					}
@@ -364,11 +491,11 @@ public class Main {
 				/*
 				 * It is time the action to be done.
 				 */
-				if (action.end == time) {
-					if (action.machine.occupied == true) {
-						action.done = true;
-						action.machine.occupied = false;
-						action.machine.action = null;
+				if (action.getEnd() == time) {
+					if (action.getMachine().isOccupied() == true) {
+						action.isDone() = true;
+						action.getMachine().isOccupied() = false;
+						action.getMachine().getAction() = null;
 					} else {
 						System.err.println("Schedule omission for: " + action);
 					}
